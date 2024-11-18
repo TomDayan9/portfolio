bbe4c2bbb87d0418cccb2af3d5a4d1802dc5b0f0
tom-dayan <tomdayan9@gmail.com>
2024-11-18 15:23:25 -0500

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio/', // Add the subdirectory name here
+  base: '/portfolio/', // Ensure this matches your repository name
   plugins: [react()],
 });
